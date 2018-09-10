fcbbb564a3cac79066e9474af839cd4ac6284d83
John Gulbronson <john@myfab5.com>
2018-09-10 02:55:31 -0400

    remove bower

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,21 +10,10 @@ module.exports = function(defaults) {
       extensions: ['js', 'css'],
       exclude: ['images'],
     },
-    emberCLIDeploy: {
-      configFile: 'config/deploy.js', // optionally specifiy a different config file
-      shouldActivate: true, // optionally call the activate hook on deploy
-    },
-    moment: {
-      includeTimezone: 'all'
-    },
     sassOptions: {
       includePaths: ['app']
     }
   });
-
-
-  app.import('bower_components/foundation/scss/foundation.scss');
-  app.import('bower_components/pluralize/pluralize.js');
 
   return app.toTree();
 };
